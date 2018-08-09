77f5b558265522b676b91e6bb8514e2f16488add
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-09 12:02:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Code your solution in this file!
+const distanceFromHqInBlocks = d => Math.abs(42-d);
+const distanceFromHqInFeet = d => distanceFromHqInBlocks(d) * 264;
+const distanceTravelledInFeet = (a,b) => Math.abs(a-b) * 264;
+const calculatesFarePrice = (a,b) => (d => d < 400 ? 0 : d < 2000 ? (d-400) * 0.02 : d < 2500 ? 25 : 'cannot travel that far')(distanceTravelledInFeet(a,b))

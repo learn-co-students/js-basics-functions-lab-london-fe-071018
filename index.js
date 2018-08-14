603967a37e9f0a0fbc2f5422574cdd69d65d0f04
Carmela Torres Molleda <carmela.torres@wework.com>
2018-08-14 11:05:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(destination) {
+  const blocks = Math.abs(destination - 42)
+  return blocks
+}
+function distanceFromHqInFeet(destination) {
+  const feet = distanceFromHqInBlocks(destination) * 264
+  return feet
+}
+
+function distanceTravelledInFeet(start, destination) {
+  const feet = Math.abs(destination - start) * 264
+  return feet
+}
+
+function calculatesFarePrice(startingBlock, endingBlock) {
+ 	const distance = distanceTravelledInFeet(startingBlock, endingBlock)
+ 	if (distance <= 400){
+		return 0;
+	} else if (distance > 400 && distance <= 2000) {
+		return (distance - 400) * 0.02;
+	} else if (distance > 2000 && distance <= 2500) {
+		return 25;
+	} else if (distance > 2500) {
+		return 'cannot travel that far';
+		}
+} 
